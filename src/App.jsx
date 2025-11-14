fbb608b58588b698cb45f4e070a0f9936e4b20f9
Jonathan <jonathanallen105@gmail.com>
2025-11-14 15:58:54 -0500

    added basename to browserrouter.

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL} >
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound/>}/>
